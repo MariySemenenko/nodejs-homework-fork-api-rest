16b68b0a92334730d1069ec93bf337c1a49e8321
MariySemenenko <mashyni92@gmail.com>
2023-08-22 19:11:26 +0300

    sendEmail

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,27 +1,23 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
- const {UKR_NET_EMAIL, UKR_NET_PASSWORD} = process.env
+const { UKR_NET_EMAIL, UKR_NET_PASSWORD } = process.env;
 
- const nodemalerCofig = {
-    host: "smtp.ukr.net",
-    port: 465, //25, 465, 2525
-    secure: true,
-    auth: {
-        user: UKR_NET_EMAIL,
-        pass: UKR_NET_PASSWORD
-    }
- }
+const nodemalerCofig = {
+  host: "smtp.ukr.net",
+  port: 465, //25, 465, 2525
+  secure: true,
+  auth: {
+    user: UKR_NET_EMAIL,
+    pass: UKR_NET_PASSWORD,
+  },
+};
 
- const transport = nodemailer.createTransport(nodemalerCofig);
+const transport = nodemailer.createTransport(nodemalerCofig);
 
- const sendEmail = async(data) => {
-    const email = {...data, from: UKR_NET_EMAIL};
-    await transport.sendMail(email)
-    return true
- }
- module.exports = sendEmail
-
-
-
-
+const sendEmail = async (data) => {
+  const email = { ...data, from: UKR_NET_EMAIL };
+  await transport.sendMail(email);
+  return true;
+};
+module.exports = sendEmail;

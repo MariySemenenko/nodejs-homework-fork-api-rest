41e9f2aa0ded26013c68939b75b7d7e70800ce93
MariySemenenko <mashyni92@gmail.com>
2023-08-28 20:25:41 +0300

    register test

---
+++ register.test.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+
+const supertest = require("supertest");
+const mongoose = require("mongoose");
+const { app } = require("./app");
+const { User } = require("./models/user");
+
+mongoose.set("strictQuery", false);
+
+const { DB_TEST_URI } = process.env;
+
+describe("register", () => {
+  beforeAll(async () => {
+    await mongoose.connect(DB_TEST_URI);
+
+    await User.deleteMany();
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect(DB_TEST_URI);
+  });
+
+  it("should register new user", async () => {
+    const response = await supertest(app).post("/api/users/register").send({
+      email: "testUser@gmail.com",
+      password: "789456",
+    });
+    expect(response.statusCode).toBe(201);
+    expect(response.body.data.user.email).toBe("testUser@gmail.com");
+  });
+
+  it("should not register the same user two times", async () => {
+    await supertest(app).post("/api/users/register").send({
+      email: "testUser@gmail.com",
+      password: "789456",
+    });
+
+    const response = await supertest(app).post("/api/users/register").send({
+      email: "testUser@gmail.com",
+      password: "789456",
+    });
+    expect(response.statusCode).toBe(409);
+  });
+});
